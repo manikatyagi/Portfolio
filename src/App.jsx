a8c90293ff378a163fa643843dd25d61013d67fb
Manika <manika.tyagi09@gmail.com>
2023-12-23 16:47:34 +0530

    commit1

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import {Navbar} from "./components/Navbar/Navbar"
 import {Intro} from "./components/intro/intro"
 import {AboutMe} from "./components/aboutMe/aboutMe"
 import { SkillsExperience } from './components/SkillsExperience/SkillsExperience';
-import {Projects} from "./components/projects/projects"
+import {Projects} from "./components/projects/Projects"
 import { Contact } from './components/contact/Contact';
 import { Education } from './components/education/education';
 
